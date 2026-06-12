46670d2ebe7dcf88be92fe997f337090d456c86e
vdk0987 <vdk0987@gmail.com>
2026-06-13 00:20:00 +0530

    fix pulumi imports, create a bucket

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import { MainBucket } from "./resources/bucket";
+
+new MainBucket({
+  name: "first-bucket",
+  product: "first-product",
+})
--- resources/bucket.ts
+++ resources/bucket.ts
@@ -1,29 +1,33 @@
-import pulumi from "@pulumi/pulumi";
-import aws from "@pulumi/aws";
+import {
+  ComponentResource,
+  CustomResourceOptions,
+  getStack,
+} from "@pulumi/pulumi";
+import { s3 } from "@pulumi/aws";
 
 type MainBucketArgs = {
   name: string;
   product: string;
 };
 
-class MainBucket extends pulumi.ComponentResource {
-  constructor(args: MainBucketArgs, opts: pulumi.CustomResourceOptions) {
+export class MainBucket extends ComponentResource {
+  constructor(args: MainBucketArgs, opts?: CustomResourceOptions) {
     const resourceName = `${args.product}-${args.name}`;
 
     super("pkg:index:MainBucket", resourceName, {}, opts);
 
-    const stack = pulumi.getStack();
+    const stack = getStack();
 
     const bucketName = `${resourceName}-${stack}`;
 
-    const bucket = new aws.s3.Bucket(args.name, {
+    const bucket = new s3.Bucket(args.name, {
       bucket: bucketName,
       tags: {
         Environment: stack,
       },
     });
 
-    new aws.s3.BucketPublicAccessBlock(args.name, {
+    new s3.BucketPublicAccessBlock(args.name, {
       bucket: bucket.id,
       blockPublicAcls: true,
       blockPublicPolicy: true,
